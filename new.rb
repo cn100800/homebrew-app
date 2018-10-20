36acbf7709dff9167b11ef10c373cf6311908220
cn <cn100800@163.com>
2018-10-20 18:34:57 +0800

    new.rb

--- new.rb
+++ new.rb
@@ -12,9 +12,9 @@ class New < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     #system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                          #{}"--disable-dependency-tracking",
+                          #{}"--disable-silent-rules",
+                          #{}"--prefix=#{prefix}"
     bin.install "new"
     # system "cmake", ".", *std_cmake_args
     #system "make", "install" # if this fails, try separate make/make install steps
