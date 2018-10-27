95efc692412aafa6a498cb24a5cdc25fa8d6cb71
cn <cn100800@163.com>
2018-10-27 16:41:44 +0800

    news

--- news.rb
+++ news.rb
@@ -1,23 +1,24 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class New < Formula
-  desc "2018.10.21.release"
+class News < Formula
+  desc "2018.10.27.release"
   homepage "https://github.com/cn100800/new/releases/latest"
-  url "https://github.com/cn100800/new/releases/download/2018.10.21.release/2018.10.21.release.tar.gz"
-  sha256 "f179a41c60f366a4d08fd83b18b76066109768d38f991e2ddd7a0114cfd15d1a"
+  url "https://github.com/cn100800/news/releases/download/2018.10.27.release/2018.10.27.release.tar.gz"
+  sha256 "fdb56f5a83224cb755e05b787ca63655eab7e09d079050e7b7df51e86f93c634"
   # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-                          #{}"--disable-dependency-tracking",
-                          #{}"--disable-silent-rules",
-                          #{}"--prefix=#{prefix}"
-    bin.install "new"
+    # system "./configure", "--disable-debug",
+                          # "--disable-dependency-tracking",
+                          # "--disable-silent-rules",
+                          # "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
+
+    bin.install "news"
+    # system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
@@ -25,7 +26,7 @@ class New < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test new`. Options passed
+    # software. Run the test with `brew test news`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
