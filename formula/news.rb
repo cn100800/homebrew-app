f5bd8ce4bf9b5b6542eee5de4a08e9d932e25ddb
cn <cn100800@163.com>
2018-12-08 01:53:34 +0800

    update.sh

--- formula/news.rb
+++ formula/news.rb
@@ -2,9 +2,9 @@
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class News < Formula
-  desc "v.20181205.0915.5b508cc"
+  desc "v.20181207.2322.ed4c850"
   homepage "https://github.com/cn100800/new/releases/latest"
-  url "https://github.com/cn100800/news/releases/download/v.20181205.0915.5b508cc/v.20181205.0915.5b508cc.tar.gz"
+  url "https://github.com/cn100800/news/releases/download/v.20181207.2322.ed4c850/v.20181207.2322.ed4c850.tar.gz"
   # sha256 "69d29344184dbb38c45a820823fee7c1f316100d7d7ba15f99df0a02f89b9efe"
   # depends_on "cmake" => :build
 
--- formula/update.sh
+++ formula/update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-tool="todo"
+tool="news todo watermark"
+
 for name in $tool
 do
-    latest_version="$(curl -sSf https://api.github.com/repos/cn100800/todo/releases | grep "v\.\d\+\.\d\+\.\w\+" | awk 'NR==2 {printf $2"\n"}' | tr -d \",)"
+    latest_version="$(curl -sSf https://api.github.com/repos/cn100800/$name/releases | grep "v\.\d\+\.\d\+\.\w\+\.zip" | awk 'NR==1 {printf $2"\n"}' | tr -d \",)"
+    latest_version=${latest_version:0:23}
     current_version=$(grep -o "v\.\d\+\.\d\+\.\w\+" "$name.rb" | head -1)
     if [[ "$latest_version" == "$current_version" ]]; then
         printf -- "%s当前版本为最新版本:%s\\n" "$name" "$latest_version"
-    else
+    elif [[ "$latest_version" != "" ]]; then
+        #statements
         sed -i '' "s/$current_version/$latest_version/g" "$name.rb"
+        printf -- "%s历史版本:%s,替换后版本:%s\\n" "$name.rb" "$current_version" "$latest_version"
     fi
 done
