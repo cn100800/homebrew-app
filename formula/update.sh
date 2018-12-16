54aa4d5c30014a5ba7c22234802c65180ab3690d
cn <cn100800@163.com>
2018-12-16 23:03:09 +0800

    update.sh

--- formula/update.sh
+++ formula/update.sh
@@ -6,6 +6,8 @@ do
     latest_version="$(curl -sSf https://api.github.com/repos/cn100800/$name/releases | grep "v\.\d\+\.\d\+\.\w\+\.zip" | awk 'NR==1 {printf $2"\n"}' | tr -d \",)"
     latest_version=${latest_version:0:23}
     current_version=$(grep -o "v\.\d\+\.\d\+\.\w\+" "$name.rb" | head -1)
+    echo $name
+
     if [[ "$latest_version" == "$current_version" ]]; then
         printf -- "%s当前版本为最新版本:%s\\n" "$name" "$latest_version"
     elif [[ "$latest_version" != "" ]]; then
